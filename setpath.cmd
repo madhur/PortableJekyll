a39d9c5ddbd87376ee0cbc11dfb06a1157ab0bd5
purana <purana@gmail.com>
2016-03-02 12:58:28 +1100

    Update setpath.cmd
    Fix typo in path I added. Missing ;

--- setpath.cmd
+++ setpath.cmd
@@ -1,7 +1,7 @@
 @echo off
 @title Portable Jekyll
 
-SET PATH=%~dp0ruby\bin;%~dp0devkit\bin;%~dp0git\bin;%~dp0devkit\mingw\bin;%~dp0curl\bin;%~dp0winscp%PATH%;
+SET PATH=%~dp0ruby\bin;%~dp0devkit\bin;%~dp0git\bin;%~dp0devkit\mingw\bin;%~dp0curl\bin;%~dp0winscp;%PATH%;
 SET SSL_CERT_FILE=%~dp0curl\bin\cacert.pem
 
 :: Header
